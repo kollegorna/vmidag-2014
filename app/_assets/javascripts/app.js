10f11cdf49cbf9470b224e6902af1e0dcd6cbcff
Per Sandström <per@helloper.com>
2014-06-12 20:26:45 +0200

    Fix match date bug

--- app/_assets/javascripts/app.js
+++ app/_assets/javascripts/app.js
@@ -1,20 +1,16 @@
 //= require bower_components/jquery/dist/jquery.min.js
 
 $(function() {
+  var match_date, previous_date;
 
   $('.match').each(function(i) {
-    //console.info('match ' + i);
-
-    var match_date, previous_date;
-
     match_date = $('.match__time',this).text().trim().substr(0,10);
-    console.log(match_date);
 
     match_time = $('.match__time',this).text().trim().substr(-5);
     $('.match__time',this).text(match_time);
 
     if (i == 0) {
-      $(this).before('<h2 class="matches__heading matches__heading--today">I dag</h2>');
+      $(this).before('<h2 class="matches__heading matches__heading--today">VM Idag</h2>');
     } else if (i == 1) {
       $(this).before('<h2 class="matches__heading matches__heading--tomorrow">I morgon</h2>');
     } else if (i > 3) {
@@ -27,9 +23,11 @@ $(function() {
   });
 
   // SVG fallback
+  /*
   if (!Modernizr.svg) {
     $("img[src$='.svg']").each(function() {
       $(this).attr("src", $(this).data('fallback'));
     });
   }
+  */
 });
