44ce14b6ec1cb6b1d157b163a6727cf008030bc4
Joakim Stai <joakimstai@gmail.com>
2014-06-14 10:10:00 +0200

    Fix bug with marking of live match
    Didn’t work for matches starting at 03:00.

--- app/_assets/javascripts/app.js
+++ app/_assets/javascripts/app.js
@@ -37,7 +37,7 @@ $(function() {
     $time.text(date.format('LT'));
 
     // Mark the match if it's currently playing.
-    if (now.isAfter(date) && now.isBefore(dateEnd)) {
+    if (now.isAfter(date.clone().subtract(10, 'hours')) && now.isBefore(dateEnd.clone().subtract(10, 'hours'))) {
       $match.addClass('live');
     }
 
